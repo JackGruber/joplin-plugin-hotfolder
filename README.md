e91ce09a0a189c77e40231452b64bc31bb033f78
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-01-25 18:41:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Joplin Backup Plugin
+
+A plugin to Monitor a locale folder and import the fiels as a new note.
+
+## Installation
+
+### Automatic
+
+- Go to `Tools > Options > Plugins`
+- Search for `Hotfolder`
+- Click Install plugin
+- Restart Joplin to enable the plugin
+
+### Manual
+
+- Download the latest released JPL package (`io.github.jackgruber.hotfolder.jpl`) from [here](https://github.com/JackGruber/joplin-plugin-hotfolder/releases/latest)
+- Close Joplin
+- Copy the downloaded JPL package in your profile `plugins` folder
+- Start Joplin
+
+## Usage
+
+First configure the Plugin under `Tools > Options > Hotfolder`!
+
+Each newly created file in the hotfolder is automatically created as a new note.
+The files are added as attachments unless the file extension is defined as `Add as text`.
+
+## Options
+
+Go to `Tools > Options > Backup`
+
+- `Hotfolder Path`: Files from this path will be imported as new notes.
+- `Add as text`: Files with this file extension are imported as text and not as attachment. Default `.txt, .md`
+
+## Changelog
+
+See [CHANGELOG.md](CHANGELOG.md)
+
+## Links
+
+- [Joplin - Getting started with plugin development](https://joplinapp.org/api/get_started/plugins/)
+- [Joplin - Plugin API reference](https://joplinapp.org/api/references/plugin_api/classes/joplin.html)
+- [Joplin - Data API reference](https://joplinapp.org/api/references/rest_api/)
+- [Joplin - Plugin examples](https://github.com/laurent22/joplin/tree/dev/packages/app-cli/tests/support/plugins)
---
+++ src/index.ts
@@ -0,0 +1,137 @@
+import joplin from "api";
+import { SettingItemType } from "api/types";
+
+const chokidar = require("chokidar");
+const fs = require("fs-extra");
+const fileType = require("file-type");
+const path = require("path");
+
+joplin.plugins.register({
+  onStart: async function () {
+    console.info("Hotfolder plugin started!");
+
+    await joplin.settings.registerSection("hotfolderSection", {
+      label: "Hotfolder",
+      iconName: "fas fa-eye",
+    });
+
+    await joplin.settings.registerSetting("hotfolderPath", {
+      value: "",
+      type: SettingItemType.String,
+      section: "hotfolderSection",
+      public: true,
+      label: "Hotfolder Path",
+    });
+
+    await joplin.settings.registerSetting("extensionsAddAsText", {
+      value: ".txt, .md",
+      type: SettingItemType.String,
+      section: "hotfolderSection",
+      public: true,
+      label: "Add as text",
+    });
+
+    await joplin.settings.registerSetting("importNotebook", {
+      value: "",
+      type: SettingItemType.String,
+      section: "hotfolderSection",
+      public: true,
+      label: "Notebook",
+    });
+
+    await registerHotfolder();
+
+    async function processFile(file: string) {
+      console.log("File", file, "has been added");
+
+      if (!fs.existsSync(file + ".lock")) {
+        const extensionsAddAsText = await joplin.settings.value(
+          "extensionsAddAsText"
+        );
+        const importNotebook = await joplin.settings.value("importNotebook");
+        const selectedFolder = await joplin.workspace.selectedFolder();
+        const notebookId = selectedFolder.id;
+
+        const mimeType = await fileType.fromFile(file);
+        const fileExt = path.extname(file);
+        const fileName = path.basename(file);
+        let newNote = null;
+        let fileBuffer = null;
+        let newResources = null;
+        let newBody = null;
+
+        if (
+          extensionsAddAsText.toLowerCase().split(",").indexOf(fileExt) !== -1
+        ) {
+          console.info("Import as Text");
+          try {
+            fileBuffer = fs.readFileSync(file);
+          } catch (e) {
+            console.error("Error on readFileSync");
+            console.error(e);
+            return;
+          }
+          newNote = await joplin.data.post(["notes"], null, {
+            body: fileBuffer.toString(),
+            title: fileName,
+            parent_id: notebookId,
+          });
+        } else {
+          console.info("Import as attachment");
+          try {
+            newResources = await joplin.data.post(
+              ["resources"],
+              null,
+              { title: fileName },
+              [
+                {
+                  path: file, // Actual file
+                },
+              ]
+            );
+          } catch (e) {
+            console.error("Error on create resources");
+            console.error(e);
+            return;
+          }
+          newBody = "[" + fileName + "](:/" + newResources.id + ")";
+          if (mimeType !== undefined && mimeType.mime.split("/")[0] === "image") {
+            newBody = "!" + newBody;
+          }
+
+          newNote = await joplin.data.post(["notes"], null, {
+            body: newBody,
+            title: fileName,
+            parent_id: notebookId,
+          });
+        }
+
+        try {
+          fs.removeSync(file);
+        } catch (e) {
+          console.error(e);
+          return;
+        }
+      }
+    }
+
+    async function registerHotfolder() {
+      const hotfolderPath = await joplin.settings.value("hotfolderPath");
+      if (hotfolderPath !== "") {
+        const watcher = chokidar
+          .watch(hotfolderPath, {
+            ignored: /(^|[\/\\])\..|\.lock$/, // ignore dotfiles and *.lock
+            persistent: true,
+            awaitWriteFinish: true,
+            depth: 0,
+            usePolling: false, // set true to successfully watch files over a network
+          })
+          .on("add", function (path) {
+            processFile(path);
+          });
+
+        var watchedPaths = watcher.getWatched();
+      }
+    }
+  },
+});
