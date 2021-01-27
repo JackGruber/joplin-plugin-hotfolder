7c30fa3c61cf7132b435508b379444106784afd1
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-01-27 18:14:36 +0100

    Dynamic hotfolder settings section

--- src/index.ts
+++ src/index.ts
@@ -10,56 +10,77 @@ joplin.plugins.register({
   onStart: async function () {
     console.info("Hotfolder plugin started!");
 
-    await joplin.settings.registerSection("hotfolderSection", {
-      label: "Hotfolder",
-      iconName: "fas fa-eye",
-    });
+    async function registerHotfolderSettings() {
+      let hotfolderNr = 0;
+      do {
+        await joplin.settings.registerSection("hotfolderSection" + (hotfolderNr == 0 ? '' : hotfolderNr), {
+          label: "Hotfolder" + (hotfolderNr == 0 ? '' : " " + (hotfolderNr + 1).toString()),
+          iconName: "fas fa-eye",
+        });
 
-    await joplin.settings.registerSetting("hotfolderPath", {
-      value: "",
-      type: SettingItemType.String,
-      section: "hotfolderSection",
-      public: true,
-      label: "Hotfolder Path",
-      description: "Please restart Joplin after a change."
-    });
+        await joplin.settings.registerSetting("hotfolderPath" + (hotfolderNr == 0 ? '' : hotfolderNr), {
+          value: "",
+          type: SettingItemType.String,
+          section: "hotfolderSection" + (hotfolderNr == 0 ? '' : hotfolderNr),
+          public: true,
+          label: "Hotfolder Path",
+          description: "Please restart Joplin after a change."
+        });
 
-    await joplin.settings.registerSetting("ignoreFiles", {
-      value: "",
-      type: SettingItemType.String,
-      section: "hotfolderSection",
-      public: true,
-      label: "Ignore Files",
-      description: "Comma separated list of files which will be ignored."
-    });
+        await joplin.settings.registerSetting("ignoreFiles" + (hotfolderNr == 0 ? '' : hotfolderNr), {
+          value: "",
+          type: SettingItemType.String,
+          section: "hotfolderSection" + (hotfolderNr == 0 ? '' : hotfolderNr),
+          public: true,
+          label: "Ignore Files",
+          description: "Comma separated list of files which will be ignored."
+        });
 
-    await joplin.settings.registerSetting("extensionsAddAsText", {
-      value: ".txt, .md",
-      type: SettingItemType.String,
-      section: "hotfolderSection",
-      public: true,
-      label: "Add as text",
-      description: "Comma separated list of file extensions, which will be imported as text."
-    });
+        await joplin.settings.registerSetting("extensionsAddAsText" + (hotfolderNr == 0 ? '' : hotfolderNr), {
+          value: ".txt, .md",
+          type: SettingItemType.String,
+          section: "hotfolderSection" + (hotfolderNr == 0 ? '' : hotfolderNr),
+          public: true,
+          label: "Add as text",
+          description: "Comma separated list of file extensions, which will be imported as text."
+        });
 
-    await joplin.settings.registerSetting("importNotebook", {
-      value: "",
-      type: SettingItemType.String,
-      section: "hotfolderSection",
-      public: true,
-      label: "Notebook",
-      description: "If no notebook is specified, the import is made to the current notebook.",
-    });
+        await joplin.settings.registerSetting("importNotebook" + (hotfolderNr == 0 ? '' : hotfolderNr), {
+          value: "",
+          type: SettingItemType.String,
+          section: "hotfolderSection" + (hotfolderNr == 0 ? '' : hotfolderNr),
+          public: true,
+          label: "Notebook",
+          description: "If no notebook is specified, the import is made to the current notebook.",
+        });
 
-    await joplin.settings.registerSetting("importTags", {
-      value: "",
-      type: SettingItemType.String,
-      section: "hotfolderSection",
-      public: true,
-      label: "Tags",
-      description: "Comma separated list of tags to be added to the note.",
-    });
+        await joplin.settings.registerSetting("importTags" + (hotfolderNr == 0 ? '' : hotfolderNr), {
+          value: "",
+          type: SettingItemType.String,
+          section: "hotfolderSection" + (hotfolderNr == 0 ? '' : hotfolderNr),
+          public: true,
+          label: "Tags",
+          description: "Comma separated list of tags to be added to the note.",
+        });
 
+        if (hotfolderNr === 0) {
+          await joplin.settings.registerSetting("hotfolderAnz", {
+            value: 1,
+            minimum: 1,
+            maximum: 10,
+            type: SettingItemType.Int,
+            section: "hotfolderSection",
+            public: true,
+            label: "Numer of Hotfolders",
+            description: "Sections appear on the left after a restart.",
+          });
+        }
+        console.log(await joplin.settings.value("hotfolderAnz"));
+        hotfolderNr++;
+      } while ( hotfolderNr < await joplin.settings.value("hotfolderAnz"));
+    }
+
+    await registerHotfolderSettings();
     await registerHotfolder();
 
     async function processFile(file: string) {
